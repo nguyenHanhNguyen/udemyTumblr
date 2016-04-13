0ea24b820c1580035d82bdeeef5019f272e39937
nguyenHanhNguyen <nhn22801@gmail.com>
2016-04-13 23:33:31 +0700

    User login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_user!, except: [:index, :show]
   #Display all postS in the descending of time
   def index
     @posts = Post.all.order('created_at DESC')
